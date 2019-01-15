058bc3f09232b186c1625d993ca0f490a06dd0c6
thebrianemory <the@brianemory.com>
2019-01-14 18:15:31 -0800

    Part 4: Add create action

--- lib/catcasts_web/controllers/session_controller.ex
+++ lib/catcasts_web/controllers/session_controller.ex
@@ -1,4 +1,18 @@
 defmodule CatcastsWeb.SessionController do
   use CatcastsWeb, :controller
   plug(Ueberauth)
+
+  alias Catcasts.User
+
+  def create(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    user_params = %{
+      token: auth.credentials.token,
+      first_name: auth.info.first_name,
+      last_name: auth.info.last_name,
+      email: auth.info.email,
+      provider: "google"
+    }
+
+    changeset = User.changeset(%User{}, user_params)
+  end
 end
