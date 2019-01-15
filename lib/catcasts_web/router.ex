d0a7daa13dbccd7bcd8a1947001a936a2d2fc843
thebrianemory <the@brianemory.com>
2019-01-14 18:42:41 -0800

    Part 5: Create and add SetUser plug

--- lib/catcasts_web/router.ex
+++ lib/catcasts_web/router.ex
@@ -7,6 +7,7 @@ defmodule CatcastsWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Catcasts.Plugs.SetUser
   end
 
   pipeline :api do
