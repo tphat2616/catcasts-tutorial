8a20426af749c45a976ecc3dc0f506d5e995423d
thebrianemory <the@brianemory.com>
2019-01-14 17:38:22 -0800

    Part 4: Add dependencies

--- mix.exs
+++ mix.exs
@@ -42,7 +42,10 @@ defmodule Catcasts.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ueberauth, "~> 0.5"},
+      {:ueberauth_google, "~> 0.7"},
+      {:poison, "~> 3.1"}
     ]
   end
 
